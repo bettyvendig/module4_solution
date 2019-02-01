dbb2cba1943891c3b640244277b07585d6ea818f
bettyvendig <46284850+bettyvendig@users.noreply.github.com>
2019-02-01 14:56:55 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -37,12 +37,15 @@ WARNING!!! WARNING!!!
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
+
+	
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -52,6 +55,7 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
+  
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -59,9 +63,11 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+   // byeSpeaker.speak(names[i]);
+	window.speakerGoodbye.speakGoodbye(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+	 window.speaker.speak(names[i]);
+   //window.helloSpeaker.speak(names[i]);
   }
 }
 
